84069f28568cd91a26641d46fb16018dd0785c7a
MUKILTEO\1717834 <mcklammt@gmail.com>
2024-01-10 12:21:07 -0800

    gun controller mostly functional, basic enemy ai has been implemented, aswell as hitboxes.

--- Q2PMB/Assets/Marcus/Player/Scripts/Weapon/Bullet.cs
+++ Q2PMB/Assets/Marcus/Player/Scripts/Weapon/Bullet.cs
@@ -9,10 +9,18 @@ public class Bullet : MonoBehaviour
 
     public void OnCollisionEnter(Collision collision)
     {
-        if(collision.transform.GetComponent<Rigidbody>())
+
+        if (collision.transform.GetComponent<Rigidbody>())
         {
             collision.transform.GetComponent<Rigidbody>().AddExplosionForce(600, collision.transform.position, 10);
             Destroy(gameObject);
         }
+
+        if(collision.transform.GetComponent<Hitbox>())
+        {
+            collision.transform.GetComponent<Hitbox>().Damage(minDamage, maxDamage, transform.position);
+            Destroy(gameObject);
+        }
+        Destroy(gameObject);
     }
 }
